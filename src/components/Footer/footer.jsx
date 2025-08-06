a6212cd00930802ac49bbec628076115dd2c8580
KingDes <desmondtate@yahoo.com>
2025-08-06 16:07:22 -0400

    Error fixes

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">© 2025 WTWR</p>
+      <nav className="footer__nav">
+        <a className="footer__link" href="https://github.com/">
+          Github
+        </a>
+        <a className="footer__link" href="https://www.figma.com/">
+          Figma
+        </a>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
