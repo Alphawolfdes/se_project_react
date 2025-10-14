436d76bae2da2452d171a8a8016c0818872e25c7
KingDes <desmondtate@yahoo.com>
2025-10-14 18:03:14 -0400

    Error fxes4

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p className="footer__name">Developed by Desmond Tate </p>
+      <nav className="footer__nav">
+        <p className="footer__copyright">2025</p>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
